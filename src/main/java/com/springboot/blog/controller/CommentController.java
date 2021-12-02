834217fc846b2d8dd71e891fedd1021eaad4c664
jackieung451 <jackieung451@gmail.com>
2021-12-01 22:09:04 -0800

    GET Comments By ID and GET All Comments by Post Id

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/")
 public class CommentController {
@@ -23,4 +25,15 @@ public class CommentController {
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDto> getCommentsByPostId(@PathVariable(value = "postId") Long postId){
+        return commentService.getCommentsByPostId(postId);
+    }
+
+    @GetMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") Long postId,
+                                                     @PathVariable(value = "id") Long commentId){
+        CommentDto commentDto = commentService.getCommentById(postId, commentId);
+        return new ResponseEntity<>(commentDto, HttpStatus.OK);
+    }
 }
--- src/main/java/com/springboot/blog/service/CommentService.java
+++ src/main/java/com/springboot/blog/service/CommentService.java
@@ -2,6 +2,12 @@ package com.springboot.blog.service;
 
 import com.springboot.blog.payload.CommentDto;
 
+import java.util.List;
+
 public interface CommentService {
     CommentDto createComment(Long postId, CommentDto commentDto);
+
+    List<CommentDto> getCommentsByPostId(Long postId);
+
+    CommentDto getCommentById(Long postId, Long commentId);
 }
